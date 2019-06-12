46cb6bbef23a2951a36c2fd3cfaaef9675d876fd
ramagurijala <35723246+ramagurijala@users.noreply.github.com>
2019-06-12 12:16:20 +0100

    added comment
    valid operations

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -10,6 +10,8 @@ exports.calculate = function(req, res) {
     res.json({ error: err.message });
   });
 
+  // valid operations
+  
   var operations = {
     'add':      function(a,b) { return a + b },
     'subtract': function(a,b) { return a - b },
